486cf34c46c62e721fc168f9b7fb4af336adac67
Aditya23524 <142024061+Aditya23524@users.noreply.github.com>
2026-02-16 18:55:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const text = "Happy Birthday My Love ishuu........ ❤️";
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 100);
+    }
+}
+
+typeEffect();
+
+function showSurprise() {
+    document.getElementById("popup").style.display = "block";
+}
+
+function closeSurprise() {
+    document.getElementById("popup").style.display = "none";
+}
